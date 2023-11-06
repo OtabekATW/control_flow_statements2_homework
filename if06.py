fe950395a24637505ed13cfb9e791e6b99aefa46
OtabekATW <otabektursunov468@gmail.com>
2023-11-07 00:21:23 +0500

    if06.py

--- if06.py
+++ if06.py
@@ -21,70 +21,18 @@ def main(n):
 
     x5 = n % 10
 
-    r = 0
-    q = 0
-
-    if x1 > x2:
-        if x1 > x3:
-            if x1 > x4:
-                if x1 > x5:
-                    r = x1
-                    q = 1
-                else:
-                    r = x5
-                    q = 5
-            else:
-                if x4 > x5:
-                    r = x4
-                    q = 4
-                else:
-                    r = x5
-                    q = 5         
-        else:
-            if x3 > x4:
-                if x3 > x5:
-                    r = x3
-                    q = 3
-                else:
-                    r = x5
-                    q = 5
-            else:
-                if x4 > x5:
-                    r = x4
-                    q = 4
-                else:
-                    r = x5
-                    q = 5
-    else:
-        if x2 > x3:
-            if x2 > x4:
-                if x2 > x5:
-                    r = x2
-                    q = 2
-                else:
-                    r = x5
-                    q = 5
-            else:
-                if x4 > x5:
-                    r = x4
-                    q = 4
-                else:
-                    r = x5
-                    q = 5
-        else:
-            if x3 > x4:
-                if x3 > x5:
-                    r = x3
-                    q = 3
-                else:
-                    r = x5
-                    q = 5
-            else:
-                if x4 > x5:
-                    r = x4
-                    q = 4
-                else:
-                    r = x5
-                    q = 5    
-
-    return q
+    idx = 0
+    mx = x5
+    if mx < x4:
+        mx = x4
+        idx = 1
+    if mx < x3:
+        mx = x3
+        idx = 2
+    if mx < x2:
+        mx = x2
+        idx = 3
+    if mx < x1:
+        mx = x1
+        idx = 4
+    return idx
