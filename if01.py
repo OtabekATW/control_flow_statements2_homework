4a4d452264dab2eccf88b5d14cdfbf842cb69462
OtabekATW <otabektursunov468@gmail.com>
2023-11-06 21:21:01 +0500

    if01.py

--- if01.py
+++ if01.py
@@ -8,13 +8,9 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    if  a > b:
-        if a > c:
-            return a
-        else:
-            return c
-    else:
-        if b > c:
-            return b
-        else:
-            return c
+    mx = a
+    if mx > b:
+        mx = b
+    if mx > c:
+        mx = c
+    return mx
